5e8b3d9caf58d3e6023e2f1e5cf251348344f0ab
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2026-05-04 12:36:08 -0300

    Reiniciando o repositório com a nova estrutura

---
+++ resources/01.Teste Spark/teste_spark.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+spark = (SparkSession.builder
+        .getOrCreate()
+)
+
+df = spark.read.csv("/data/storage/E-Commerce/Produto.csv", header=True, inferSchema=True)
+
+df.show()
+
+spark.stop()
